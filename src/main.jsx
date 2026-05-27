bc8e081423f8e59faa4c79584e29a85992484af6
seankizy <seankizy@gmail.com>
2026-05-28 01:57:49 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MacroTracker from "./MacroTracker";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MacroTracker />
+  </React.StrictMode>
+);
